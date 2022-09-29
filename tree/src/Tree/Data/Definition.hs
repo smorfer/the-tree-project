fcfd9f41e90934300f898cf56e5c03b8c23dfe2b
Samuel Glogger <smorfi98@gmail.com>
2022-09-29 22:39:09 +0200

    Add TreeableF class with TreeF instance

--- tree/src/Tree/Data/Definition.hs
+++ tree/src/Tree/Data/Definition.hs
@@ -1,23 +1,17 @@
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE DerivingVia #-}
-{-# LANGUAGE InstanceSigs #-}
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DerivingVia       #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs             #-}
+{-# LANGUAGE InstanceSigs      #-}
+{-# LANGUAGE RankNTypes        #-}
 
 module Tree.Data.Definition (Tree(..), TreeF(..)) where
 
-import           Tree.Data.Definition.Treeable (Treeable(..))
+import           Tree.Data.Definition.Treeable (Treeable (..), TreeableF (..))
+import           Tree.Data.Utils               (Fillable (..))
 
 data Tree where
   Tree :: [Tree] -> Tree
   deriving Show
-
-data TreeF a where
-  TreeF :: a -> [TreeF a] -> TreeF a
-
-instance Functor TreeF where
-  fmap :: (a -> b) -> TreeF a -> TreeF b
-  fmap f (TreeF a tfs) = TreeF (f a) (fmap (fmap f) tfs)
 
 instance Treeable Tree where
   mergeT :: [Tree] -> Tree
@@ -25,3 +19,18 @@ instance Treeable Tree where
 
   splitT :: Tree -> [Tree]
   splitT (Tree trs) = trs
+
+data TreeF a where
+  TreeF :: a -> [TreeF a] -> TreeF a
+
+instance TreeableF TreeF where
+  mergeTF :: Fillable a => [TreeF a] -> TreeF a
+  mergeTF = TreeF fill
+  splitTF :: TreeF a -> [TreeF a]
+  splitTF (TreeF _ tfs) = tfs
+
+
+instance Functor TreeF where
+  fmap :: (a -> b) -> TreeF a -> TreeF b
+  fmap f (TreeF a tfs) = TreeF (f a) (fmap (fmap f) tfs)
+
--- tree/src/Tree/Data/Definition/Treeable.hs
+++ tree/src/Tree/Data/Definition/Treeable.hs
@@ -1,5 +1,12 @@
-module Tree.Data.Definition.Treeable (Treeable(..)) where
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE RankNTypes          #-}
+module Tree.Data.Definition.Treeable ( Treeable(..), TreeableF(..)) where
+import           Tree.Data.Utils (Fillable)
 
 class Treeable t where
   mergeT :: [t] -> t
   splitT :: t -> [t]
+
+class TreeableF tf where
+  splitTF :: (Fillable a) => tf a -> [tf a]
+  mergeTF :: (Fillable a) => [tf a] -> tf a
