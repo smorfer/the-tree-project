ab8918904caeb04b72014cd3303ac41a37774fd8
Samuel Glogger <smorfi98@gmail.com>
2022-09-30 02:29:11 +0200

    Refactor Transformation helper methods into where clauses

--- tree/src/Tree/Control/Transformation.hs
+++ tree/src/Tree/Control/Transformation.hs
@@ -1,19 +1,23 @@
 module Tree.Control.Transformation (transformTtoTFI, transformTFItoDT, transformTtoDT) where
 
-import           Tree.Data.Definition.DepthTree (DepthTree(..))
-import           Tree.Data.Definition (Tree(..), TreeF(TreeF))
+import           Tree.Data.Definition           (Tree (..), TreeF (TreeF))
+import           Tree.Data.Definition.DepthTree (DepthTree (..))
 
-tfT2TFI :: Integer -> Tree -> TreeF Integer
-tfT2TFI n (Tree trs) = TreeF n (tfT2TFI (n + 1) <$> trs)
 
 transformTtoTFI :: Tree -> TreeF Integer
 transformTtoTFI = tfT2TFI 0
+ where
+  tfT2TFI :: Integer -> Tree -> TreeF Integer
+  tfT2TFI n (Tree trs) = TreeF n (tfT2TFI (n + 1) <$> trs)
 
-tfTFI2DT :: TreeF Integer -> [Integer]
-tfTFI2DT (TreeF n tfs) = n:(tfTFI2DT =<< tfs)
+transformTFItoT :: TreeF Integer -> Tree
+transformTFItoT (TreeF _ tfs) = Tree (transformTFItoT <$> tfs)
 
 transformTFItoDT :: TreeF Integer -> DepthTree
 transformTFItoDT = DepthTree . tfTFI2DT
+ where
+  tfTFI2DT :: TreeF Integer -> [Integer]
+  tfTFI2DT (TreeF n tfs) = n : (tfTFI2DT =<< tfs)
 
 transformTtoDT :: Tree -> DepthTree
 transformTtoDT = transformTFItoDT . transformTtoTFI
