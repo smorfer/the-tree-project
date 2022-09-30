4ad28f7d4291d597069c8dd003da5b160ba6ee35
Samuel Glogger <smorfi98@gmail.com>
2022-09-30 02:34:48 +0200

    Finalize and fix DepthTree definition

--- tree/src/Tree/Data/Definition/DepthTree.hs
+++ tree/src/Tree/Data/Definition/DepthTree.hs
@@ -1,4 +1,26 @@
+{-# LANGUAGE DerivingVia  #-}
+{-# LANGUAGE GADTs        #-}
+{-# LANGUAGE InstanceSigs #-}
 module Tree.Data.Definition.DepthTree (DepthTree(..)) where
+import           Data.List.Split               (split, startsWith)
+import           Tree.Data.Definition.Treeable (Treeable (..))
 
-newtype DepthTree = DepthTree [Integer]
-  deriving Show
+newtype DepthTreeF a where
+  DepthTreeF :: [a] -> DepthTreeF a
+  deriving Functor via []
+
+newtype DepthTree = DepthTree { getDT :: [Integer]}
+
+instance Show DepthTree where
+  show :: DepthTree -> String
+  show = show . getDT
+
+
+
+instance Treeable DepthTree where
+  mergeT :: [DepthTree] -> DepthTree
+  mergeT dts = DepthTree (0 : (succ <$> (getDT =<< dts)))
+  splitT :: DepthTree -> [DepthTree]
+  splitT (DepthTree [])       = error "splitT: DepthTree empty"
+  splitT (DepthTree [0]) = []
+  splitT (DepthTree (_ : is)) = DepthTree <$> (fmap pred <$> split (startsWith [1]) is )
